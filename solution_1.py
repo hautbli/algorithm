90262a00130cb8c3327ae0511f762ef1a8d5a243
withHappy <hap2.dev@gmail.com>
2020-11-18 17:51:06 +0900

    refactor: mv dir

--- solution_1.py
+++
@@ -1,5 +0,0 @@
-def solution_1(n):
-    answer = 0
-    for i in map(int, list(str(n))):
-        answer += i
-    return answer
--- solution_test.py
+++
@@ -1,7 +0,0 @@
-from solution_2 import solution_2
-from solution_1 import solution_1
-
-a = 626331
-
-# print(solution_1(a))
-print(solution_2(a))
