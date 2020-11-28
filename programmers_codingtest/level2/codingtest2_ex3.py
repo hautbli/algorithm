e96030472ff714a45277b99987c1e16bdc4c974a
withHappy <hap2.dev@gmail.com>
2020-11-28 23:04:39 +0900

    add codingtest2_ex3 time_overrun

--- programmers_codingtest/level2/codingtest2_ex3.py
+++ programmers_codingtest/level2/codingtest2_ex3.py
@@ -1,4 +1,17 @@
 # 큰 수 만들기 (탐욕법)
-def solution(number, k):
-    answer = ''
-    return answer
+from itertools import permutations
+
+
+def solution(numbers):
+    answer = 0
+    numbers = list(map(str, numbers))
+    numbers = list(permutations(numbers, len(numbers)))
+    for n in numbers:
+        num = int(''.join(n))
+        if answer < num:
+            answer = num
+
+    return str(answer)
+
+
+print(solution([3, 30, 34, 5, 9]))
