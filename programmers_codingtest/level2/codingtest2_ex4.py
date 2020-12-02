97cba9ed1d463e0f88cfb96eb1d7c3a98318a9fd
withHappy <hap2.dev@gmail.com>
2020-12-02 18:05:55 +0900

    add codingtest2_ex4_example

--- programmers_codingtest/level2/codingtest2_ex4.py
+++ programmers_codingtest/level2/codingtest2_ex4.py
@@ -27,3 +27,16 @@ def solution(priorities, location):
 
 # 몇 번 째로 출력하는지
 print(solution([1, 1, 9, 1, 1, 1], 0) == 5)  # 4 3 2 2 1 -> 4
+
+
+def solution(priorities, location):
+    queue = [(i, p) for i, p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
