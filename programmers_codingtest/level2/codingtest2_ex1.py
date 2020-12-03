1e4cbe7e90025bdadaece96e3406ce97c3ad9664
withHappy <hap2.dev@gmail.com>
2020-12-03 17:15:02 +0900

    update codingtest2_ex1_example

--- programmers_codingtest/level2/codingtest2_ex1.py
+++ programmers_codingtest/level2/codingtest2_ex1.py
@@ -3,43 +3,46 @@ from string import ascii_uppercase
 
 
 def solution(name):
-    alpha_list = list(ascii_uppercase)
     answer = 0
-    # 위아래
-    for ix, n in enumerate(name):
-        if n == 'A':
-            continue
+    name = list(name)
+    idx = 0
+    while (True):
+        right_A_count = 1
+        left_A_count = 1
+        if name[idx] != 'A':
+            # idx가 A가 아닌경우
+            # 위아래 -> 더 가까운 것
+            updown = min(ord(name[idx]) - ord('A'), ord('Z') - ord(name[idx]) + 1)
+            answer += updown
 
-        idx = alpha_list.index(n)
-        _idx = len(alpha_list) - idx
+        name[idx] = 'A'
+        # print(name)
+        if name == ['A'] * len(name): break  # A완성되면 종료!
 
-        if idx < _idx:
-            answer += idx
+        # 연속된 A의 개수가 방향으로 가야 최소횟수로 이동할 수 있음
+        for i in range(1, len(name)):
+            if name[idx + i] == 'A':
+                right_A_count += 1
+            else:
+                break
+
+        for i in range(1, len(name)):
+            if name[idx - i] == 'A':
+                left_A_count += 1
+            else:
+                break
+
+        if right_A_count > left_A_count:
+            # 왼쪽이 A가 아닌게 더 가까울 때
+            answer += left_A_count
+            idx -= left_A_count
         else:
-            answer += _idx
-
-    answer += len(name) - 1
-
-    # 근접한 A개수 구하기
-    l = 0
-    r = 0
-    for i in range(1, len(name)):
-        if name[i] == 'A':
-            l += 1
-        else:
-            break
-    for i in range(1, len(name)):
-        if name[-i] == 'A':
-            r += 1
-        else:
-            break
-    # l or r
-    print(l,r)
-    if l < r:
-        answer -= r
-    else:
-        answer -= l
+            # 오른쪽이 A가 아닌게 더 가까울 때나 둘이 같을 때
+            answer += right_A_count
+            idx += right_A_count
     return answer
 
 
-print(solution("AAAAAJZA"))
+print(solution("JDABSAFE"))
+
+'BBAAAAAAAABBBBBBAA'
