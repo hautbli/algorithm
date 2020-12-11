a825fad5eda4ae0535ecbe56dc344285598737ed
withHappy <hap2.dev@gmail.com>
2020-12-11 09:20:17 +0900

    update codingtest2_ex8

--- programmers_codingtest/level2/codingtest2_ex8.py
+++ programmers_codingtest/level2/codingtest2_ex8.py
@@ -1,7 +1,17 @@
 # 구명보트
 def solution(people, limit):
     answer = 0
+    people.sort()
+    front = 0
+    back = len(people) - 1
+
+    while front <= back:
+        answer += 1
+        if people[front] + people[back] <= limit:
+            front += 1
+
+        back -= 1
     return answer
 
 
-print(solution([70, 50, 80, 50], 100))
+print(solution([40,50,60,70,80], 100))
