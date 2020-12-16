148a23c79b4a786647c388f132eda5c97fdfd6f9
withHappy <hap2.dev@gmail.com>
2020-12-16 16:28:49 +0900

    update codingtest2_ex9

--- programmers_codingtest/level2/codingtest2_ex9.py
+++ programmers_codingtest/level2/codingtest2_ex9.py
@@ -32,4 +32,29 @@ def solution(progresses, speeds):
     return answer
 
 
+# print(solution([93, 30, 55], [1, 30, 5]))
+
+
+def solution(progresses, speeds):
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses) > 0:
+        if (progresses[0] + time * speeds[0]) >= 100:
+            # 100이 넘었을 경우
+            # pop을 하고 count +1
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            # time은 계속 1씩 더해짐 근데 더해도 100보다 작을 경우
+            # 그 전까지의 count는 append
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+
+
 print(solution([93, 30, 55], [1, 30, 5]))
