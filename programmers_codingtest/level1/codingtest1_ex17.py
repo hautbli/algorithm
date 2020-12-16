6422a1c103666e1ca1b2da6f0d39abfaaddcbbb0
withHappy <hap2.dev@gmail.com>
2020-12-16 14:43:45 +0900

    update codingtest1_ex17

--- programmers_codingtest/level1/codingtest1_ex17.py
+++ programmers_codingtest/level1/codingtest1_ex17.py
@@ -1,6 +1,8 @@
 # 시저암호
 
 def solution(s, n):
+    # n이 ord(z)를 몇배나 큰 경우는 통과 못 함
+    # -> 모듈러 사용!!
     answer = ''
     for i in s:
         if i == ' ':
@@ -16,3 +18,14 @@ def solution(s, n):
 
 
 print(solution('a B z', 4))
+
+
+def solution2(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
+
+    return "".join(s)
