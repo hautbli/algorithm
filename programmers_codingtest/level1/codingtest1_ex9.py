d433c0672f8e6e7e8275f71af1253462c03661ef
withHappy <hap2.dev@gmail.com>
2020-12-17 22:11:11 +0900

    update codingtest2_ex12

--- programmers_codingtest/level1/codingtest1_ex9.py
+++ programmers_codingtest/level1/codingtest1_ex9.py
@@ -1,8 +1,6 @@
 # 소수 찾기
 
 def solution(n):
-    answer = 0
-
     pn = set(range(2, n + 1))
     for i in range(2, n + 1):
         pn -= set(range(2 * i, n + 1, i))
---
+++ programmers_codingtest/level2/codingtest2_ex12.py
@@ -0,0 +1,35 @@
+# 소수 찾기 (완전탐색)
+from itertools import permutations
+
+
+def is_prime(n):
+    # 소수이면 True 반환
+    if n < 2: return False
+    if n in (2, 3): return True
+    if n % 2 == 0 or n % 3 == 0: return False
+    if n < 9: return True
+    k, l = 5, n ** 0.5
+    while k <= l:
+        if n % k == 0 or n % (k + 2) == 0:
+            return False
+        k += 6
+    return True
+
+
+def solution(numbers):
+    numbers_list = []
+    for i in range(1, len(numbers) + 1):
+        # 조합을 int(스트링으로 넣으면 011,11이 구분이 안됨) -> 리스트에 추가
+        tmp = permutations(numbers, i)
+        for n in tmp:
+            tmp_int = int(''.join(n))
+            numbers_list.append(tmp_int)
+
+    count = 0
+    for i in set(numbers_list):  # 조합 중복 제거 -> set
+        if is_prime(int(i)):
+            count += 1
+    return count
+
+
+print(solution('011'))
